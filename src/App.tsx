67584e91dffdc3f5ee94197a5d0091b384a704d2
xanoosh <xanoosh@gmail.com>
2021-07-07 20:55:28 +0200

    added onclick event with state change (adding value to string)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import './App.css';
 import Button from './Button';
+import { useState } from 'react';
+
+//propably gonna use eval on '=' button to calculate stuff and put it in calculation state
 
 function App() {
+  const [calculation, setCalculation] = useState<string>('');
+  const handleClick = (e: MouseEvent) => {
+    const val = e.currentTarget as HTMLButtonElement;
+    console.log(val.textContent);
+    setCalculation((prev) => prev + val.textContent);
+    // setCalculation((prev) => 's');
+  };
   return (
     <div className="App">
-      <h2>my calculator app</h2>
-      <Button val="1" />
+      <h2>Current state: {calculation}</h2>
+      <Button val="1" click={handleClick} />
     </div>
   );
 }
--- src/Button.tsx
+++ src/Button.tsx
@@ -2,17 +2,21 @@ import React from 'react';
 
 interface Props {
   val: string;
+  click: any;
   isNumber?: boolean;
   isClicked?: boolean;
 }
 
 const Button: React.FC<Props> = ({
   val,
+  click,
   isNumber = false,
   isClicked = false,
 }) => {
   return (
-    <button className={isNumber && isClicked ? 'inactive' : ''}>{val}</button>
+    <button onClick={click} className={isNumber && isClicked ? 'inactive' : ''}>
+      {val}
+    </button>
   );
 };
 
