d4c1f0c05dcc9d21703adda0c2c33c8971ff5314
xanoosh <xanoosh@gmail.com>
2021-07-07 21:04:03 +0200

    mutiple button components, function to calculate result wit eval()

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,27 @@ import { useState } from 'react';
 
 function App() {
   const [calculation, setCalculation] = useState<string>('');
-  const handleClick = (e: MouseEvent) => {
+  const handleCalculation = (e: MouseEvent) => {
     const val = e.currentTarget as HTMLButtonElement;
     console.log(val.textContent);
     setCalculation((prev) => prev + val.textContent);
     // setCalculation((prev) => 's');
   };
+  const handleResult = (e: MouseEvent) => {
+    const val = e.currentTarget as HTMLButtonElement;
+    console.log(val.textContent);
+    const calculate = calculation;
+    setCalculation((prev) => eval(prev));
+  };
   return (
     <div className="App">
       <h2>Current state: {calculation}</h2>
-      <Button val="1" click={handleClick} />
+      <Button val="1" click={handleCalculation} />
+      <Button val="2" click={handleCalculation} />
+      <Button val="3" click={handleCalculation} />
+      <Button val="+" click={handleCalculation} />
+      <Button val="-" click={handleCalculation} />
+      <Button val="=" click={handleResult} />
     </div>
   );
 }
