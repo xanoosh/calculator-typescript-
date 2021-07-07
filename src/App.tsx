3657d099184c128a4bdbdc6a956204f17fc58994
xanoosh <xanoosh@gmail.com>
2021-07-07 19:15:08 +0200

    textfield component wit props validation in typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import TextField from './TextField';
 
 function App() {
   return (
     <div className="App">
       <h2>my calculator app</h2>
+      <TextField text="text" />
     </div>
   );
 }
